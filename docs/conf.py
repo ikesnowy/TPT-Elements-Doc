a2ba5758cf6e2a0bcb84051945018b9583205584
沈星繁 <ikesnowy@outlook.com>
2018-12-26 12:02:18 +0800

    delete build folder

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'TPT 元素说明'
-copyright = '2017, Ike'
+copyright = '2019, Ike'
 author = 'Ike'
 
 # The version info for the project you're documenting, acts as replacement for
